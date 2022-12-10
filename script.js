67c840f5545aeb8f197cc627fdff6f6fbab07ba1
Nikhil Gupta <jerrycodes29@gmail.com>
2022-12-10 17:05:26 +0530

    Added functionality of guessing right number

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const $ = (selector) => document.querySelector(selector)
+const randomNumber = Math.trunc(Math.random() * 30) + 1
+
+const winningNumber = $('.number')
+winningNumber.textContent = randomNumber
+
+const msgElement = $('.message')
+const displayMessage = function (message) {
+    msgElement.textContent = message
+}
+
+$('#checkBtn').addEventListener('click', function () {
+    const guessValue = Number($('.guess').value)
+    if (!guessValue) displayMessage('⛔ Please input some number!')
+    else if (guessValue === randomNumber) {
+        displayMessage('✌🏼 Yay! Congratulations!')
+
+        $('body').style.backgroundColor = '#68B984'
+        winningNumber.style.backgroundColor = '#ffffff'
+        winningNumber.style.borderStyle = 'solid'
+    }
+})
